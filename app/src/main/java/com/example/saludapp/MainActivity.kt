9846dd2ad9cf06c06d17b2c2284ed5a53464712e
rafmorcar2 <rafamc999@gmail.com>
2024-10-22 13:55:43 +0200

    acabar en casa

---
+++ app/src/main/java/com/example/saludapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.saludapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+            val btnHello: AppCompatButton = findViewById(R.id.btnHello)
+
+            btnHello.setOnClickListener {
+                Log.i("Test", "Botón pulsado")
+            }
+        }
+    }
+}
